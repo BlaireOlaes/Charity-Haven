d42f2da1bee8e3d2a41cd55b62238098a4f031a6
Cla <clarizzesaisay@gmail.com>
2024-05-14 02:02:01 +0800

    Profile-contact-app.jsx-index.css

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -30,6 +31,38 @@ function App() {
       </p>
     </>
   )
+=======
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Contacts from "./screens/Contacts";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import SignIn from "./pages/SigngIn";
+import CreateAccount from "./pages/CreateAccount";
+import Home from "./pages/Home";
+import AdminManage from "./pages/AdminManage";
+import PostManage from "./pages/PostManage";
+import Profile from "./pages/Profile";
+
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/create" element={<CreateAccount />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/adminmanage" element={<AdminManage />} />
+                <Route path="/postmanage" element={<PostManage />} />
+                <Route path="/profile" element={<Profile />} />
+
+            </Routes>
+        </Router>
+    );
+>>>>>>> Stashed changes
 }
 
 export default App
---
+++ react/src/screens/Contacts.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../css/index.css';
+
+function Contacts() {
+    return (
+        <div className="contact-wrapper">
+            <div className="contact-info">
+                    <h1>Contact Informations</h1>
+                    <hr className="left" />
+                            <p2>Contact us through:</p2>
+                            <hr className="right" />
+                 
+                    <h2>Gcash number: 090000001</h2>
+                    <h2>Email: bjohaven@gmail.com</h2>
+                    <h2>Facebook: facebook.com/bjo</h2>
+                </div>
+                </div>
+    );
+}
+
+export default Contacts;
